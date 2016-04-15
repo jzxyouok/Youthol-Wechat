8d907b62070ccf2ff0781ebd72888c1e60faf9c3
Youthink <hufy3651@163.com>
2016-04-15 11:10:08 +0800

    add failure.php

--- application/views/lib/failure.php
+++ application/views/lib/failure.php
@@ -15,42 +15,21 @@
 	<title>登录失败</title>
 </head>
 <body>
-	<header class="index-header center">
-		<h1>登录图书馆</h1>
-	</header>
-	<?php echo validation_errors(); ?>
-
-    <?php echo form_open('lib/login'); ?>
-		<div class="weui_cells weui_cells_form">
-		    <div class="weui_cell">
-		        <div class="weui_cell_hd">
-		            <label class="weui_label">学号</label>
-		        </div>
-		        <div class="weui_cell_bd weui_cell_primary">
-		            <input class="weui_input" type="number" name="sdutnum" placeholder="请输入学号">
-		        </div>
-		    </div>
-		    <div class="weui_cell">
-		        <div class="weui_cell_hd">
-		            <label class="weui_label">密码</label>
-		        </div>
-		        <div class="weui_cell_bd weui_cell_primary">
-		            <input class="weui_input" type="password" name="password" placeholder="初始密码为">
-		        </div>
-		    </div>
-		    <div class="weui_cell weui_cells_checkbox">
-			    <label class="weui_cell weui_check_label" for="s11">
-			        <div class="weui_cell_hd">
-			            <input type="checkbox" class="weui_check" name="remember_pass" id="s11" checked="checked">
-			            <i class="weui_icon_checked"></i>
-			        </div>
-			        <div class="weui_cell_bd weui_cell_primary">
-			            <p>记住密码</p>
-			        </div>
-			    </label>
-			</div>
-	  		<button type="submit" class="weui_btn weui_btn_primary">登录</button>
-		</div>
+		<div class="weui_msg">
+	    <div class="weui_icon_area"><i class="weui_icon_warn weui_icon_msg"></i></div>
+	    <div class="weui_text_area">
+	        <h2 class="weui_msg_title">登录失败</h2>
+	        <p class="weui_msg_desc">请确认学号和密码是否正确</p>
+	    </div>
+	    <div class="weui_opr_area">
+	        <p class="weui_btn_area">
+	            <a href="<?php echo site_url()."/lib/login" ?>" class="weui_btn weui_btn_primary">返回</a>
+	        </p>
+	    </div>
+	    <div class="weui_extra_area">
+	        <a href="">查看详情</a>
+	    </div>
+	</div>
 	<script src="<?php echo base_url()."public/less/dist/less.min.js" ?>"></script>
 </body>
 </html>
