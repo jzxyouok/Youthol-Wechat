c2a10fc7e48d3a7cf5f5cf03f61297b87b5e6ee1
Youthink <hufy3651@163.com>
2016-04-17 18:49:48 +0800

    add success.php

---
+++ application/views/lib/success.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<head>
+	<meta charset="UTF-8">
+	<meta name="keywords" contnt="">
+	<meta name="description" content="">
+	<meta name="author" content="hufangyun.com">
+	<meta name="viewport" content="width=device-width" initial-scale="1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<link rel="icon" href="/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="<?php echo base_url()."public/css/weui.min.css" ?>"/>
+	<!--<link rel="stylesheet" href="<?php echo base_url()."public/css/style.css" ?>"/>-->
+	<link rel="stylesheet/less" href="<?php echo base_url()."public/less/style.less" ?>" />
+	<title>智慧校园|图书馆</title>
+</head>
+<body>
+		<div class="weui_msg">
+	    <div class="weui_icon_area"><i class="weui_icon_success weui_icon_msg"></i></div>
+	    <div class="weui_text_area">
+	        <h2 class="weui_msg_title">操作成功</h2>
+	        <p class="weui_msg_desc">感谢您的使用</p>
+	    </div>
+	    <div class="weui_opr_area">
+	        <p class="weui_btn_area">
+	            <a href="<?php echo site_url() ?>" class="weui_btn weui_btn_primary">返回</a>
+	        </p>
+	    </div>
+	    <div class="weui_extra_area">
+	        <a href="">查看详情</a>
+	    </div>
+	</div>
+	<script src="<?php echo base_url()."public/less/dist/less.min.js" ?>"></script>
+</body>
+</html>
